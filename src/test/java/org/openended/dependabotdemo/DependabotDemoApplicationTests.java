ae94680484e8e55984675a04bf1fff6b725f4810
Jens Fischer <jens.fischer@otto.de>
2019-06-24 16:17:22 +0200

    kotlin2java

---
+++ src/test/java/org/openended/dependabotdemo/DependabotDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package org.openended.dependabotdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DependabotDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
